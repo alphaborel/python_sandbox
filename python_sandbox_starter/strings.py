6c574467df58dd787dbaecd05dc20d574d61e2bf
Tony B <aaborel@gmail.com>
2018-12-13 16:13:29 -0700

    Managing first changes

--- python_sandbox_starter/strings.py
+++ python_sandbox_starter/strings.py
@@ -1,6 +1,13 @@
 # Strings in python are surrounded by either single or double quotation marks. Let's look at string formatting and some string methods
 
+name = 'Tony'
+age = 37
+
+# Concatenate (you can only concat string variables)
+print('Hello, my name is ' + name)
 
 # String Formatting
+
+# Arguments by position
 
 # String Methods
--- python_sandbox_starter/variables.py
+++ python_sandbox_starter/variables.py
@@ -13,3 +13,22 @@ VARIABLE RULES:
   - Must start with a letter or an underscore
   - Can have numbers but can not start with one
 """
+# Note: No semicolins for statements in Python
+
+# x = 1           # int
+# y = 2.5         # float
+# name = 'tony'   # str
+# is_cool = True  # bool
+
+# Multiple assignment
+x, y, name, is_cool = (1, 2.5, 'Tony', True)
+
+# Basic math
+a = x + y
+print(a)
+
+# casting (changing variable type)
+x = str(x)
+y = int(y)
+
+print(type(y), y)
